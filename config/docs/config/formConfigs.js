96df5640ba2c81ae00043738c94fdce3a101c185
NrdcProjects <projects12.new@gmail.com>
2022-09-25 22:19:03 +0530

    turborepo added

---
+++ config/docs/config/formConfigs.js
@@ -0,0 +1,13 @@
+
+export const lumpsumFormConfig = [
+    {fieldTitle: 'Lumpsum Amount R&D Institution', sourceField: 'lumpsum1', percentageField: 'lumpsumPercentageDev', result: ''},
+    {fieldTitle: 'Lumpsum Amount NRDC', sourceField: 'lumpsum1', percentageField: 'lumpsumPercentage', result: ''},
+    {fieldTitle: 'GST Result R&D Institution', sourceField: 'gstAmount1', percentageField: 'gstPercentageDev', result: ''},
+    {fieldTitle: 'GST Result NRDC', sourceField: 'gstAmount1', percentageField: 'gstPercentage', result: ''}
+   
+]
+
+export const royaltyFormConfig = [
+    {fieldTitle: 'Royalty Result R&D Institution', sourceField: 'sale1', percentageField: 'royaltyPercentageDev', result: ''},
+    {fieldTitle: 'Royalty Result NRDC', sourceField: 'sale1', percentageField: 'royaltyPercentage', result: ''}
+]
